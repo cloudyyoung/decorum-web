5ccf7cb53fd82097f7a993eb9e9e94f5a500b952
Yunfan Yang <cloudy.young@outlook.com>
2024-10-06 01:07:49 -0600

    Add route

---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+
+export const Home = () => {
+  return (
+    <div className="container is-max-tablet">
+      <section className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="">
+            <p className="title is-size-2 barlow-condensed-bold">Décorum-Gen</p>
+            <p className="subtitle biorhyme-400">An unofficial generator for the board game</p>
+
+            <div className="buttons are-medium">
+              <button className="button is-white">Create game</button>
+              <button className="button is-white">Join game</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Home
