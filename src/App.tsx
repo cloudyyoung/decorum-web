0d82691e8049dbffd663f460f730bd0445fa6dc0
Yunfan Yang <cloudy.young@outlook.com>
2024-10-06 00:49:55 -0600

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ export const App = () => {
         <section className="hero is-info is-fullheight">
           <div className="hero-body">
             <div className="">
-              <p className="title">Décorum Generator</p>
-              <p className="subtitle">An unofficial generator for the board game.</p>
+              <p className="title is-size-2 barlow-condensed-bold">Décorum-Gen</p>
+              <p className="subtitle biorhyme-400">An unofficial generator for the board game</p>
 
               <div className="buttons are-medium">
                 <button className="button is-white">Create game</button>
