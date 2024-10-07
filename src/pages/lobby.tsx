20a334766cb20bd33b7f3604709516cefd9891b0
Yunfan Yang <cloudy.young@outlook.com>
2024-10-07 01:10:22 -0600

    Update lobby.tsx

--- src/pages/lobby.tsx
+++ src/pages/lobby.tsx
@@ -112,7 +112,7 @@ export const Lobby = () => {
       <div className="sticky bottom-0 left-0 right-0 p-4 bg-white">
         <div className="flex gap-2 justify-between max-w-screen-sm mx-auto">
           <Button color="white" href="/">Leave</Button>
-          <Button disabled={selectedPlayer === null} onClick={onEnterGame}>
+          <Button disabled={!selectedPlayer} onClick={onEnterGame}>
             Enter Game
             <ArrowRightIcon />
           </Button>
