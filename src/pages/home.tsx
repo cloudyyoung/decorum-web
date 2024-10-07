f3304be7a8290b05568faa7b8f47631e18a63e16
Yunfan Yang <cloudy.young@outlook.com>
2024-10-07 00:36:24 -0600

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import { Heading, Subheading } from "../components/heading"
 import { Button } from "../components/button"
+import { PlusIcon, ArrowUpRightIcon } from "@heroicons/react/16/solid"
 
 export const Home = () => {
+  const navigate = useNavigate()
+  const onJoinGame = () => navigate("/join")
+
   return (
     <section className="flex items-center jusityf-center h-screen p-4">
       <div className="w-full">
@@ -9,8 +14,14 @@ export const Home = () => {
         <Subheading>An unofficial level generator for the board game</Subheading>
 
         <div className="flex gap-2 mt-6">
-          <Button>Create game</Button>
-          <Button color="white" href="/join">Join game</Button>
+          <Button>
+            Create
+            <PlusIcon />
+          </Button>
+          <Button color="white" onClick={onJoinGame}>
+            Join
+            <ArrowUpRightIcon />
+          </Button>
         </div>
       </div>
     </section>
