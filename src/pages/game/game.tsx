fd6881301cde33f6a9075972ef967d7b39650e62
Yunfan Yang <cloudy.young@outlook.com>
2024-10-06 21:23:36 -0600

    Extract tab component

--- src/pages/game/game.tsx
+++ src/pages/game/game.tsx
@@ -1,51 +1,18 @@
 import { useState } from "react"
 import HouseSetup from "./house_setup"
 import Condition from "./condition"
-import clsx from "clsx"
 import { Heading, Subheading } from "../../components/heading"
+import Tab, { TabOption } from "./tab"
 
 
 export const Game = () => {
-  const [tab, setTab] = useState<"setup" | "conditions">("setup")
+  const [tab, setTab] = useState<TabOption>("setup")
 
   return (
     <>
       <section className="">
         <div className="flex justify-center sticky top-0">
-          <nav aria-label="Tabs" className="isolate flex divide-x divide-gray-200 rounded-lg shadow-md w-36 mt-2 mb-4">
-            <button
-              className={clsx(
-                tab === "setup" ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                'rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-1.5 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
-              )}
-              onClick={() => setTab("setup")}
-            >
-              <span>Setup</span>
-              <span
-                aria-hidden="true"
-                className={clsx(
-                  tab === "setup" ? 'bg-player1' : 'bg-transparent',
-                  'absolute inset-x-0 bottom-0 h-0.5',
-                )}
-              />
-            </button>
-            <button
-              className={clsx(
-                tab === "conditions" ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                'rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-1.5 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
-              )}
-              onClick={() => setTab("conditions")}
-            >
-              <span>Conditions</span>
-              <span
-                aria-hidden="true"
-                className={clsx(
-                  tab === "conditions" ? 'bg-player1' : 'bg-transparent',
-                  'absolute inset-x-0 bottom-0 h-0.5',
-                )}
-              />
-            </button>
-          </nav>
+          <Tab tab={tab} setTab={setTab} />
         </div>
 
         {tab === "setup" && <HouseSetup />}
---
+++ src/pages/game/tab.tsx
@@ -0,0 +1,49 @@
+import clsx from "clsx"
+
+export type TabOption = "setup" | "conditions"
+
+export interface TabProps {
+  tab: TabOption
+  setTab: (tab: TabOption) => void
+}
+
+export const Tab = ({ tab, setTab }: TabProps) => {
+  return (
+    <nav aria-label="Tabs" className="isolate flex divide-x divide-gray-200 rounded-lg shadow-md w-36 mt-2 mb-4">
+      <button
+        className={clsx(
+          tab === "setup" ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+          'rounded-l-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-1.5 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
+        )}
+        onClick={() => setTab("setup")}
+      >
+        <span>Setup</span>
+        <span
+          aria-hidden="true"
+          className={clsx(
+            tab === "setup" ? 'bg-player1' : 'bg-transparent',
+            'absolute inset-x-0 bottom-0 h-0.5',
+          )}
+        />
+      </button>
+      <button
+        className={clsx(
+          tab === "conditions" ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+          'rounded-r-lg group relative min-w-0 flex-1 overflow-hidden bg-white py-1.5 text-center text-sm font-medium hover:bg-gray-50 focus:z-10',
+        )}
+        onClick={() => setTab("conditions")}
+      >
+        <span>Conditions</span>
+        <span
+          aria-hidden="true"
+          className={clsx(
+            tab === "conditions" ? 'bg-player1' : 'bg-transparent',
+            'absolute inset-x-0 bottom-0 h-0.5',
+          )}
+        />
+      </button>
+    </nav>
+  )
+}
+
+export default Tab
